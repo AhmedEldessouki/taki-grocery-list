44d3f72dbb78199733e4526beb99cd0a70dad63b
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-05-12 14:16:39 +0300

    cleaning icons

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,45 +1,4 @@
-import React, {useEffect, useState} from 'react'
-
-// import {Twitter} from '@material-ui/icons'
-
-function Arrow({
-  direction = 'right',
-}: {
-  direction: 'left' | 'right' | 'down' | 'up'
-}) {
-  const [arrow, setArrow] = useState(() => {
-    if (direction === 'left') return 'rotate(90deg)'
-    else if (direction === 'right') return 'rotate(270deg)'
-    else if (direction === 'up') return 'rotate(180deg)'
-    return 'rotate(0deg)'
-  })
-  useEffect(() => {
-    setArrow(() => {
-      if (direction === 'left') return 'rotate(90deg)'
-      else if (direction === 'right') return 'rotate(270deg)'
-      else if (direction === 'up') return 'rotate(180deg)'
-      return 'rotate(0deg)'
-    })
-  }, [direction])
-  return (
-    <div
-      style={{
-        width: '18px',
-        transform: arrow,
-        transition: `transform 200ms linear`,
-      }}
-    >
-      <svg viewBox="0 0 320 512">
-        <path
-          d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
-          fill="var(--white)"
-          className=""
-          rotate="180deg"
-        />
-      </svg>
-    </div>
-  )
-}
+import React from 'react'
 
 function XIcon() {
   return (
@@ -52,4 +11,4 @@ function XIcon() {
   )
 }
 
-export {Arrow, XIcon}
+export {XIcon}
