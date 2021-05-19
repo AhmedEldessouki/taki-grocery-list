df8c928ee93045871e04cbca91046977de8a314a
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-05-19 17:41:47 +0300

    adding serviceWorker and registering it in index

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {QueryClient, QueryClientProvider} from 'react-query'
 import App from './App'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import {AuthProvider} from './context/auth'
 import './index.css'
 
@@ -17,3 +18,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register()
