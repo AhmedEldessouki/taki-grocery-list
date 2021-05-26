6b1d33bac8a5404971807d1b72c5e3d41bd9d7bf
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-05-26 16:35:05 +0300

    using faker to create User Data

---
+++ src/test/userDB.ts
@@ -0,0 +1,13 @@
+import faker from 'faker'
+import type {UserDataType} from '../../types/user'
+
+function generateUserData(): UserDataType {
+  return {
+    name: faker.name.firstName() + ' ' + faker.name.lastName(),
+    userId: faker.datatype.uuid(),
+    listName: [faker.random.words()],
+    email: faker.internet.email(),
+  }
+}
+
+export {generateUserData}
