862bbe2cb80efc9fb5e3a61ca23c66a4a3169e29
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-09-15 20:42:05 +0300

    addList: modeifing tests commenting the last text since we no longer clean the input value but the value that will be submitted

--- src/components/forms/addList.test.tsx
+++ src/components/forms/addList.test.tsx
@@ -4,21 +4,46 @@ import {generateListName} from '../../test/groceryDB'
 import generateUserData from '../../test/userDB'
 import {render, screen, userEvent} from '../../test/utils'
 import AddList from './addList'
+// import ListName from './listName'
 
 const user: UserDataType = generateUserData()
 
 function MyWrapper() {
   const [listArray, setListArray] = React.useState<Array<string>>([])
   return (
-    <AddList
-      componentName="grocery"
-      userId={user.userId}
-      oldList={user.listName}
-      setArrayChange={setListArray}
-      listArray={listArray}
-    />
+    <>
+      <AddList
+        userId={user.userId}
+        oldList={user.listName}
+        setArrayChange={setListArray}
+        listArray={listArray}
+      />
+    </>
   )
 }
+// ! here we need to extract the submitting function to replace it with another
+// ! to avoid mocking which will unhelpful in this situation
+// function MyWrapperWithLists() {
+//   const [listArray, setListArray] = React.useState<Array<string>>([])
+//   return (
+//     <>
+//       <AddList
+//         userId={user.userId}
+//         oldList={user.listName}
+//         setArrayChange={setListArray}
+//         listArray={listArray}
+//       />
+//       {listArray.map((list, i) => (
+//         <ListName
+//           key={`${i * 2}`}
+//           userLists={listArray}
+//           index={i}
+//           userID="userData.userId"
+//         />
+//       ))}
+//     </>
+//   )
+// }
 
 test('Adding the first Input:: User has lest than 3 lists', async () => {
   await render(<MyWrapper />)
@@ -26,7 +51,7 @@ test('Adding the first Input:: User has lest than 3 lists', async () => {
   userEvent.click(screen.getByText(/Add/i))
   expect(
     screen.getByRole('textbox', {
-      name: /grocery list/i,
+      name: /new list name/i,
     }),
   ).toBeInTheDocument()
 })
@@ -51,73 +76,60 @@ test('typing in input', async () => {
 
   expect(
     screen.getByRole('textbox', {
-      name: /grocery list/i,
+      name: /new list name/i,
     }),
   ).toBeInTheDocument()
 
   const someListName = generateListName()
   userEvent.type(
     screen.getByRole('textbox', {
-      name: /grocery list/i,
+      name: /new list name/i,
     }),
     someListName,
   )
 
   expect(
     screen.getByRole('textbox', {
-      name: /grocery list/i,
+      name: /new list name/i,
     }),
   ).toHaveDisplayValue(someListName)
 })
 
-test('Input extra white space cleaning', async () => {
-  let listArray: string[] = []
-  const setListArray = (arr: string[]) => {
-    listArray = arr
-  }
-  // The ListArray is receiving an array with 1 empty string because after clicking
-  // on Add btn component will not update unless something else happens that updates the component
-  // so we lie to component from the start and after executing "setListArray" we check if it got cleaned or not
-  await render(
-    <AddList
-      componentName="grocery"
-      userId={user.userId}
-      oldList={user.listName}
-      setArrayChange={
-        setListArray as React.Dispatch<React.SetStateAction<string[]>>
-      }
-      listArray={['']}
-    />,
-  )
+// test('Input extra white space cleaning', async () => {
+//   // The ListArray is receiving an array with 1 empty string because after clicking
+//   // on Add btn component will not update unless something else happens that updates the component
+//   // so we lie to component from the start and after executing "setListArray" we check if it got cleaned or not
+//   await render(<MyWrapperWithLists />)
 
-  userEvent.click(screen.getByText(/Add grocery list/i))
+//   userEvent.click(screen.getByText(/Add grocery list/i))
 
-  expect(
-    screen.getByRole('textbox', {
-      name: /grocery list/i,
-    }),
-  ).toBeInTheDocument()
+//   expect(
+//     screen.getByRole('textbox', {
+//       name: /new list name/i,
+//     }),
+//   ).toBeInTheDocument()
 
-  const someCleanListName = generateListName()
-  const someListName = `                      ${someCleanListName}          + daad +                                    `
+//   const someCleanListName = generateListName()
+//   const someListName = `                      ${someCleanListName}          + daad +                                    `
 
-  userEvent.type(
-    screen.getByRole('textbox', {
-      name: /grocery list/i,
-    }),
-    someListName,
-  )
+//   userEvent.type(
+//     screen.getByRole('textbox', {
+//       name: /new list name/i,
+//     }),
+//     someListName,
+//   )
+//   expect(
+//     screen.getByRole('textbox', {
+//       name: /new list name/i,
+//     }),
+//   ).toHaveDisplayValue(someListName)
 
-  expect(
-    screen.getByRole('textbox', {
-      name: /grocery list/i,
-    }),
-  ).toHaveDisplayValue(someListName)
-
-  userEvent.click(
-    screen.getByRole('button', {
-      name: /submit/i,
-    }),
-  )
-  expect(listArray).toContain(`${someCleanListName.toLowerCase()} + daad +`)
-})
+//   userEvent.click(
+//     screen.getByRole('button', {
+//       name: /submit/i,
+//     }),
+//   )
+//   // expect(
+//   //   screen.getByText(`${someCleanListName.toLowerCase()} + daad +`),
+//   // ).toBeInTheDocument()
+// })
