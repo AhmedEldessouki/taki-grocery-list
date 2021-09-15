61eb3db5c3f84f27d161d53e72bb0dbb6c25365a
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-09-15 18:24:55 +0300

    styling lang form

--- src/components/langSelect.tsx
+++ src/components/langSelect.tsx
@@ -1,14 +1,23 @@
+import styled from '@emotion/styled'
 import React from 'react'
 import {useLang} from '../context/lang'
 
+const $Select = styled.select`
+  background: transparent;
+  border: 2px solid rgba(0, 0, 0, 0.2);
+  border-radius: var(--roundness);
+  color: var(--mattGray);
+  font-weight: 500;
+  padding: 5px 7px;
+`
 function LangSelect() {
   const {locale, selectLang} = useLang()
 
   return (
-    <select name="lang" value={locale} onChange={selectLang}>
+    <$Select name="lang" value={locale} onChange={selectLang}>
       <option value="en">En</option>
       <option value="tr">Tr</option>
-    </select>
+    </$Select>
   )
 }
 
